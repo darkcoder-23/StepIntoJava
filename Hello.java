7d07e14f1d16341a053b51b03cd05e560305b7ea
Md Shayan Raza <developershayan23@gmail.com>
2024-09-28 02:12:51 +0530

    Add The First Java Code

---
+++ Hello.java
@@ -0,0 +1,25 @@
+class Hello {
+    public static void main(String a[]) {
+        // Variabale
+        int num1 = 5;
+        int num2 = 5;
+        int sum = num1 + num2;
+
+        double y = 5.6;
+        float x = 4.5f;
+        boolean flag = true;
+        char ch = 'k';
+
+        int n = 10_00_00_000;   // same as 10000000
+
+
+        char c = 'a';
+        c++;
+
+
+        // Print - ln for new line
+        System.out.println("Hello World!");
+        System.out.println(5+7);
+        System.out.println(c);
+    }
+}
